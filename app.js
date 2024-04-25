04efbe205d7180c92471c64b7cadc11efc86208f
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-04-25 16:38:44 -0400

    Added Images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const emojis = ["😍", "😍", "👍", "👍", "🌟", "🌟", "❤️", "❤️", "🍕", "🍕","🎉", "🎉", "😊", "😊", "🚀", "🚀"];
+const emojis = ["🌎", "🌎", "🏈", "🏈", "🌟", "🌟", "❤️", "❤️", "🍕", "🍕","🎸", "🎸", "🎮", "🎮", "🚀", "🚀"];
 
-var shuffleEmojis = emojis.sort(() => (Math.random() > .5) ? 2: -1);
-for (var i =0; i<emojis.length; i++){
+let shuffleEmojis = emojis.sort(() => (Math.random() > .5) ? 2: -1);
+for (let i =0; i<emojis.length; i++){
   let box = document.createElement('div')
   box.className = 'item';
   box.innerHTML = shuffleEmojis[i]
